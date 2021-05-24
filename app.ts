c96aead32838c074e939f1d8baba2bdddc6caa68
Rizwan Zaheer <rzaheer@datavalue.ae>
2021-05-24 20:29:04 +0500

    adding public folder with index.html file

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
-import { Application } from "https://deno.land/x/abc@v1.3.1/mod.ts";
+import { Application, Context } from "https://deno.land/x/abc@v1.3.1/mod.ts";
 
 const app = new Application();
 
 const port = 3000;
 
-app.start({port})
+// Routes
+app.get("/", async (ctx: Context) => {
+  console.log("get routes is calling!!!");
+  await ctx.file("./public/index.html");
+});
 
-console.log('Welcome in deno world!');
+// server start/listen
+app.start({ port });
+
+console.log("Welcome in deno world!");
